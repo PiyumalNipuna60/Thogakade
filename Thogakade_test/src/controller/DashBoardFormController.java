bef05f9e7dedd15a1706f2cfcad7ec498be9e9a2
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-09 00:02:11 +0530

    Added Ui & Controller Class

---
+++ Thogakade_test/src/controller/DashBoardFormController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashBoardFormController {
+    public void SaveCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SaveCustomerForm");
+    }
+
+    public void SearchCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SearchCustomerForm");
+    }
+
+    public void UpdateCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("UpdateCustomerForm");
+    }
+
+    public void DeleteCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DeleteCustomerForm");
+    }
+
+    public void LoadAllCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoadAllCustomerForm");
+    }
+
+    public void setUi(String URI) throws IOException {
+        Parent parent=FXMLLoader.load(getClass().getResource("../views/"+URI+".fxml"));
+        Stage stage=new Stage();
+        stage.setScene(new Scene(parent));
+//        stage.setTitle(URI);
+        stage.show();
+    }
+}
---
+++ Thogakade_test/src/controller/SaveCustomerFormController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SaveCustomerFormController {
+    public Button btnSave;
+    public TextField txtCustomerId;
+    public TextField txtCustomerAddress;
+    public TextField txtCustomerName;
+    public TextField txtCustomerSalary;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+}
