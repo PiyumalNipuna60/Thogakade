712302efce47679f17b4bb669441d18985d70b6f
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-10 01:08:50 +0530

    Modify Customer Search  by JDBC Connection

--- Thogakade_test/src/controller/SearchCustomerFormController.java
+++ Thogakade_test/src/controller/SearchCustomerFormController.java
@@ -1,11 +1,11 @@
 package controller;
 
-import db.DataSet;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import model.Customer;
+
+import java.sql.*;
 
 public class SearchCustomerFormController {
     public Button btnSearch;
@@ -23,7 +23,7 @@ public class SearchCustomerFormController {
     }
 
     public void Search(){
-        for (Customer temp: DataSet.customerArray) {
+        /*for (Customer temp: DataSet.customerArray) {
             if (temp.getCusId().equals(txtCustomerId.getText())){
                 txtCustomerName.setText(temp.getCusName());
                 txtCustomerAddress.setText(temp.getCusAddress());
@@ -31,7 +31,24 @@ public class SearchCustomerFormController {
                 return;
             }
         }
-        new Alert(Alert.AlertType.WARNING,"Empty Result!..").show();
+        new Alert(Alert.AlertType.WARNING,"Empty Result!..").show();*/
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "1234");
+            String sql = "SELECT * FROM Customer WHERE id='" + txtCustomerId.getText() + "'";
+            Statement sta = con.createStatement();
+            ResultSet resultSet = sta.executeQuery(sql);
+            if (resultSet.next()){
+                txtCustomerName.setText(resultSet.getString(2));
+                txtCustomerAddress.setText(resultSet.getString(3));
+                txtCustomerSalary.setText(String.valueOf(resultSet.getDouble(4)));
+            }else {
+                new Alert(Alert.AlertType.WARNING,"Empty Result!..").show();
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
