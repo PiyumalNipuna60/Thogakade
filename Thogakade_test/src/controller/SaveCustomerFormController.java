15364b4ad8d423905a23e8037057ca4949efb11f
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-10 01:00:18 +0530

    Customer Save by JDBC Connection

--- Thogakade_test/src/controller/SaveCustomerFormController.java
+++ Thogakade_test/src/controller/SaveCustomerFormController.java
@@ -7,6 +7,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import model.Customer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class SaveCustomerFormController {
     public Button btnSave;
     public TextField txtCustomerId;
@@ -15,13 +20,30 @@ public class SaveCustomerFormController {
     public TextField txtCustomerSalary;
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
-        Customer customer=new Customer(txtCustomerId.getText(),txtCustomerName.getText(),txtCustomerAddress.getText(),Double.parseDouble(txtCustomerSalary.getText()));
-        DataSet.customerArray.add(customer);
+        Customer c=new Customer(txtCustomerId.getText(),txtCustomerName.getText(),txtCustomerAddress.getText(),Double.parseDouble(txtCustomerSalary.getText()));
+        /*DataSet.customerArray.add(customer);
         if (DataSet.customerArray.add(customer)){
             new Alert(Alert.AlertType.CONFIRMATION,"Save Customer!..").show();
         }else {
             new Alert(Alert.AlertType.CONFIRMATION,"Something went Wrong!..").show();
+        }*/
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade","root","1234");
+            String sql="INSERT INTO Customer VALUES('"+c.getCusId()+"','"+c.getCusName()+"','"+c.getCusAddress()+"','"+c.getCusSalary()+"')";
+            Statement sta = con.createStatement();
+            if (sta.executeUpdate(sql)>0){
+                new Alert(Alert.AlertType.CONFIRMATION,"Save Customer!..").show();
+            }else {
+                new Alert(Alert.AlertType.CONFIRMATION,"Something went Wrong!..").show();
+            }
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
+
     }
 
 
