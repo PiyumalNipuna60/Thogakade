3e5dbf1158109b83ea31accd2292a46417a98a75
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-09 15:20:15 +0530

    Search Class Complete

--- Thogakade_test/src/controller/SearchCustomerFormController.java
+++ Thogakade_test/src/controller/SearchCustomerFormController.java
@@ -1,4 +1,37 @@
 package controller;
 
+import db.DataSet;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import model.Customer;
+
 public class SearchCustomerFormController {
+    public Button btnSearch;
+    public TextField txtCustomerId;
+    public TextField txtCustomerName;
+    public TextField txtCustomerAddress;
+    public TextField txtCustomerSalary;
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+        Search();
+    }
+
+    public void txtSearchOnAction(ActionEvent actionEvent) {
+        Search();
+    }
+
+    public void Search(){
+        for (Customer temp: DataSet.customerArray) {
+            if (temp.getCusId().equals(txtCustomerId.getText())){
+                txtCustomerName.setText(temp.getCusName());
+                txtCustomerAddress.setText(temp.getCusAddress());
+                txtCustomerSalary.setText(String.valueOf(temp.getCusSalary()));
+                return;
+            }
+        }
+        new Alert(Alert.AlertType.WARNING,"Empty Result!..").show();
+    }
+
 }
