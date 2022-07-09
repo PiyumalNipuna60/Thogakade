aec9254e5e8d8209887c196911d3b47ac06b802c
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-10 00:49:06 +0530

    Load All Custome ui Create

--- Thogakade_test/src/controller/LoadAllCustomerFormController.java
+++ Thogakade_test/src/controller/LoadAllCustomerFormController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class LoadAllCustomerFormController {
+    public TableView tblCustomer;
+    public TableColumn colCusId;
+    public TableColumn colCusName;
+    public TableColumn colCusAddress;
+    public TableColumn colCusSalary;
 }
