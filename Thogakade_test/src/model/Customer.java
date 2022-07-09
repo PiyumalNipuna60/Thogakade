78d38008c1ad3b602d878c644802db6324bfb81b
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-09 14:50:15 +0530

    Added Customer model

---
+++ Thogakade_test/src/model/Customer.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Customer {
+    String cusId;
+    String cusName;
+    String cusAddress;
+    String cusSalary;
+
+    public Customer() {
+    }
+
+    public Customer(String cusId, String cusName, String cusAddress, String cusSalary) {
+        this.cusId = cusId;
+        this.cusName = cusName;
+        this.cusAddress = cusAddress;
+        this.cusSalary = cusSalary;
+    }
+
+    public String getCusId() {
+        return cusId;
+    }
+
+    public void setCusId(String cusId) {
+        this.cusId = cusId;
+    }
+
+    public String getCusName() {
+        return cusName;
+    }
+
+    public void setCusName(String cusName) {
+        this.cusName = cusName;
+    }
+
+    public String getCusAddress() {
+        return cusAddress;
+    }
+
+    public void setCusAddress(String cusAddress) {
+        this.cusAddress = cusAddress;
+    }
+
+    public String getCusSalary() {
+        return cusSalary;
+    }
+
+    public void setCusSalary(String cusSalary) {
+        this.cusSalary = cusSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "cusId='" + cusId + '\'' +
+                ", cusName='" + cusName + '\'' +
+                ", cusAddress='" + cusAddress + '\'' +
+                ", cusSalary='" + cusSalary + '\'' +
+                '}';
+    }
+}
