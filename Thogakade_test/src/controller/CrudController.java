28efc0fc43762ee37466730564af2af3c9a22591
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-12 00:20:48 +0530

    Added CrudContoller Class

---
+++ Thogakade_test/src/controller/CrudController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CrudController {
+    public static ArrayList<String> getCustomerId() throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = CrudUtil.execute("SELECT id FROM Customer");
+        ArrayList<String> arrayList=new ArrayList<>();
+        while (resultSet.next()){
+            arrayList.add(resultSet.getString(1));
+        }
+        return arrayList;
+    }
+
+
+}
