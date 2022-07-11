24ba460e8ed1c50af3c993c98715f7f41fb0af95
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-07-12 00:04:10 +0530

    Fix PlaceOrder UI

--- Thogakade_test/src/controller/PlaceOrderFormController.java
+++ Thogakade_test/src/controller/PlaceOrderFormController.java
@@ -1,4 +1,40 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class PlaceOrderFormController {
+
+    public Label lblDate;
+    public Label lblTime;
+    public ComboBox cmdCusId;
+    public TextField txtName;
+    public TextField txtxAddress;
+    public TextField txtSalary;
+    public ComboBox cmdItemCode;
+    public TextField txtDescription;
+    public TextField txtItemQtyOnHand;
+    public TextField txtUnitPrice;
+    public TextField txtQty;
+    public Button btnRemoveItem;
+    public TableView tblCart;
+    public TableColumn colCode;
+    public TableColumn colDescription;
+    public TableColumn colUnitPrice;
+    public TableColumn colQty;
+    public TableColumn colTotalCost;
+    public TableColumn colButton;
+    public Label lblTotal;
+
+    public void CustomerAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlaceOrder(ActionEvent actionEvent) {
+    }
+
+    public void addtoCartOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRemoveItemOnAction(ActionEvent actionEvent) {
+    }
 }
